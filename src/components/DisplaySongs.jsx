0516e1d972806529e5094f1624cae5785ef0dded
Ben Bissett <benjaminbissett@gmail.com>
2021-11-05 13:04:46 -0600

    App now displays all songs in the library on a table.

--- src/components/DisplaySongs.jsx
+++ src/components/DisplaySongs.jsx
@@ -1,12 +1,36 @@
 import React from 'react';
 
 const DisplaySongs = (props) => {
+    let rows = []
+    for (let i = 0; i <props.songs.length; i++) {
+        let id = i;
+        let title = props.songs[i].title;
+        let artist = props.songs[i].artist;
+        let album = props.songs[i].album;
+        let genre = props.songs[i].genre;
+        let release_date = props.songs[i].release_date;
+        rows.push(<tr>
+            <td>{id}</td>
+            <td>{title}</td>
+            <td>{artist}</td>
+            <td>{album}</td>
+            <td>{genre}</td>
+            <td>{release_date}</td>
+        </tr>)
+    }
     return ( 
         <div>
-            <h3>{props.songs[0].title} - {props.songs[0].artist}</h3>
-            <p>{props.songs[0].album}</p>
-            <p>Genre: {props.songs[0].genre}</p>
-            <p>Release Date: {props.songs[0].release_date}</p>
+            <table>
+                <tr>
+                    <td><strong>ID</strong></td>
+                    <td><strong>Song</strong></td>
+                    <td><strong>Artist</strong></td>
+                    <td><strong>Album</strong></td>
+                    <td><strong>Genre</strong></td>
+                    <td><strong>Release Date</strong></td>
+                </tr>
+                {rows}
+            </table>
         </div>
      );
 }
