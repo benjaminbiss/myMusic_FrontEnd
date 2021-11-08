a7580e71a9015e9815d57f45559984bf937834b3
Ben Bissett <benjaminbissett@gmail.com>
2021-11-08 12:14:08 -0700

    removed the .css files, I was not pleased with the appearance I had from them. I will add bootstrap at a later time.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import DisplaySongs from './components/DisplaySongs/DisplaySongs';
 import CreateSong from './components/CreateSong/CreateSong';
-// import './App.css'
 
 class App extends Component {
     constructor(props) {
@@ -28,7 +27,7 @@ class App extends Component {
         this.deleteRow();
         this.getSongs();
     }
-    
+
     deleteRow = async (id) => {
         try {
             await axios.delete(`http://127.0.0.1:8000/music/${id}/`);
--- src/components/CreateSong/CreateSong.jsx
+++ src/components/CreateSong/CreateSong.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-// import './CreateSong.css';
 
 
 class CreateSong extends Component {
